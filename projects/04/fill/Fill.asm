2318cccc746181f749fc7131452dbdd4f83eed57
cincinnatus <c.cincinnat@gmail.com>
2020-03-28 12:07:23 +0300

    04: fill the screen on key pressed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,77 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+// state: 1: uninitialized, 0: white, -1: black
+@state
+M=1
+
+// fill the screen with white
+@WHITE
+0; JMP
+
+// loop forever
+(LOOP)
+    // check if a key is pressed
+    @KBD
+    D=M;
+    @WHITE
+    D; JEQ
+
+(BLACK)
+    @color
+    M=-1
+    @FILL
+    0; JMP
+
+(WHITE)
+    @color
+    M=0
+
+(FILL)
+    // if color == state: goto LOOP
+    @color
+    D=M
+    @state
+    D=D-M;
+    @LOOP
+    D; JEQ
+
+    // while i > 0
+    @8192
+    D=A
+    @i
+    M=D
+(FILL_LOOP)
+        // D=--i
+        @i
+        M=M-1
+        D=M
+
+        // addr = SCREEN + i
+        @SCREEN
+        D=D+A
+        @addr
+        M=D
+
+        // *addr = color
+        @color
+        D=M
+        @addr
+        A=M
+        M=D
+
+        @i
+        D=M;
+        @FILL_LOOP
+        D; JNE
+
+    // state = color
+    @color
+    D=M
+    @state
+    M=D
+
+@LOOP
+0; JMP
